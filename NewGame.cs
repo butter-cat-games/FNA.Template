89aec962c8c4cd6aeb5e7055cd60b987bdcd46d1
Daniel Crenna <daniel.crenna@gmail.com>
2020-01-11 15:20:06 -0500

    add templating source files

---
+++ NewGame.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NewGameTemplate
+{
+    public class NewGame : Game
+    {
+        private readonly GraphicsDevice graphicsDevice;
+
+        public NewGame()
+        {
+            var graphicsDeviceManager = new GraphicsDeviceManager(this);
+            graphicsDevice = graphicsDeviceManager.GraphicsDevice;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            graphicsDevice.Clear(Color.CornflowerBlue);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NewGameTemplate
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Bootstrap.Initialize_FNA();
+            using var game = new NewGame();
+            game.Run();
+        }
+    }
+}
